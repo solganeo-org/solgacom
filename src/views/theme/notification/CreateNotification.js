0b9036ce232a3c0e6438df1fcf49faa29609a06e
LyesRABHI <l.rahbi@solganeo.com>
2021-11-15 12:29:23 +0100

    changement Notification

--- src/views/theme/notification/CreateNotification.js
+++ src/views/theme/notification/CreateNotification.js
@@ -20,7 +20,11 @@ const Notification = () => {
   // Variables
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
-  const [clients, setClients] = useState([])
+  const [urlButton, seturlButton] = useState('')
+  const [urlRed, setUrlRed] = useState('')
+  const [urlImage, setUrlImage] = useState('')
+  const [customer, setCustomer] = useState([])
+  const contact = JSON.parse(sessionStorage.getItem('contact'))
   const currentSite =
     sessionStorage.getItem('currentSite') != null &&
     sessionStorage.getItem('currentSite') !== 'undefined'
@@ -36,50 +40,32 @@ const Notification = () => {
         .get(process.env.REACT_APP_ENDPOINT + '/api/sites-customers/site-id/' + idSite)
         .then((resp) => {
           if (resp.status == 200) {
+            console.log(contact.id)
             let clientsResponse = resp.data
-            setClients(clientsResponse)
+            setCustomer(clientsResponse)
           }
         })
     }
   }, [])
 
   // On click button Send
-  const sendNotification = (e) => {
+  const createNotification = (e) => {
     e.preventDefault()
-
-    let lastClient = clients[clients.length - 1]
-    console.log(lastClient.endpoint)
-
-    const vapidDetails = {
-      mailto: 'mailto:r.zuniga@solganeo.com',
-      publicKey: currentSite.public_key,
-      privateKey: currentSite.private_key,
-    }
-    let pushSubscription = {
-      endpoint: lastClient.endpoint,
-      keys: {
-        p256dh: lastClient.key_p256dh,
-        auth: lastClient.key_auth,
-      },
-    }
-    const payload = JSON.stringify({
-      title: title,
-      body: content,
-      icon: 'https://drive.google.com/file/d/1HPH-xIeDWhUB9O2-Fc6anfPU-QdeYPJk/view?usp=sharing',
-      url1: 'http://google.com/',
-      url2: 'https://www.yahoo.com/',
-      actionName: 'archive',
-      actiontitle: 'LOL',
-    })
-
     axios
-      .post(process.env.REACT_APP_ENDPOINT + '/api/send-notification', {
-        payload: payload,
-        vapidDetails: vapidDetails,
-        pushSubscription: pushSubscription,
+      .post(process.env.REACT_APP_ENDPOINT + '/api/notification', {
+        title: title,
+        content: content,
+        id_contact: contact.id,
+        urlImage: urlImage,
+        urlButton: urlButton,
+        urlRed: urlRed,
+        status: 'Draft',
+        active: 1,
       })
-      .then((resp) => {
-        console.log(resp.data)
+      .then(function (response) {
+        if (response.status == 200) {
+          window.location.reload()
+        }
       })
   }
 
@@ -91,7 +77,7 @@ const Notification = () => {
           <CCardHeader>Create Notification</CCardHeader>
           <CCardBody>
             <CListGroup flush>
-              <CForm onSubmit={sendNotification}>
+              <CForm onSubmit={createNotification}>
                 <CInputGroup className="mb-3">
                   <CFormControl
                     type="text"
@@ -108,10 +94,34 @@ const Notification = () => {
                     onChange={(e) => setContent(e.target.value)}
                   />
                 </CInputGroup>
+                <CInputGroup className="mb-3">
+                  <CFormControl
+                    type="text"
+                    placeholder="URL Image"
+                    autoComplete="URL Image"
+                    onChange={(e) => setUrlImage(e.target.value)}
+                  />
+                </CInputGroup>
+                <CInputGroup className="mb-4">
+                  <CFormControl
+                    type="text"
+                    placeholder="URL Button"
+                    autoComplete="URL Button"
+                    onChange={(e) => seturlButton(e.target.value)}
+                  />
+                </CInputGroup>
+                <CInputGroup className="mb-4">
+                  <CFormControl
+                    type="text"
+                    placeholder="URL Reddirection"
+                    autoComplete="URL Redirection"
+                    onChange={(e) => setUrlRed(e.target.value)}
+                  />
+                </CInputGroup>
                 <CRow>
                   <CCol xs="6">
                     <CButton type="submit" color="success" className="px-4">
-                      Send
+                      Create
                     </CButton>
                   </CCol>
                 </CRow>
